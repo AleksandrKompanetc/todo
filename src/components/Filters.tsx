67e1328bbadba003e15aa9090126f46d4cde9eec
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-05 23:33:30 +0300

    created Filters component

---
+++ src/components/Filters.tsx
@@ -0,0 +1,10 @@
+import { Filter } from '../types'
+
+interface FiltersProps {
+  currentFilter: Filter
+  onChange: (filter: Filter) => void
+}
+
+export default function Filters({ currentFilter, onChange }: FiltersProps) {
+
+}
