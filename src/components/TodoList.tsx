5467c6a0c27ac07853b323ffff89043b37aa2c51
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-06 23:55:59 +0300

    creating main component TodoList

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,8 @@
+import TodoItem from './TodoItem'
+import { Todo } from '../types'
+
+export default function TodoList() {
+  return (
+    <div></div>
+  )
+}
--- src/components/TodoStats.tsx
+++ src/components/TodoStats.tsx
@@ -4,6 +4,8 @@ interface TodoStatsProps {
 
 export default function TodoStats({ activeCount }: TodoStatsProps) {
   return (
-    <div></div>
+    <div className="text-center mt-6 text-gray-500">
+      Remains to be done: <span className="font-semibold text-gray-700">{activeCount}</span>
+    </div>
   )
 }
