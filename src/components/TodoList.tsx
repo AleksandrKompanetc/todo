8c35a41473724bdda846faa1b5df9cf4cad21c47
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-07 17:51:01 +0300

    created TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,8 +1,34 @@
 import TodoItem from './TodoItem'
 import { Todo } from '../types'
 
-export default function TodoList() {
+interface TodoListProps {
+  todos: Todo[]
+  onToggle: (id: number) => void
+  onDelete: (id: number) => void
+}
+
+export default function TodoList({ todos,onToggle, onDelete }: TodoListProps) {
+  if (todos.length === 0) {
+    return (
+      <div className='bg-white rounded-2xl shadow-sm py-12'>
+        <p className='text-center text-gray-500 text-lg'>
+          No tasks yet! Add some tasks to get started.
+        </p>
+      </div>
+    )
+  }
   return (
-    <div></div>
+    <div className='bg-white rounded-2xl shadow-sm overflow-hidden'>
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem 
+            key={todo.id}
+            todo={todo}
+            onToggle={onToggle}
+            onDelete={onDelete}
+          />
+        ))}
+      </ul>
+    </div>
   )
 }
