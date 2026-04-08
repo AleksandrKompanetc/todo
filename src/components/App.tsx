5001d7cb2074d10d2f666d8827e76bacd762b1af
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-08 23:20:13 +0300

    changed App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,22 @@
 import { useState, useEffect } from 'react'
+import { Todo, Filter } from '../types'
+
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [filter, setFilter] = useState<Filter>('all')
+
+  useEffect(() => {
+    const savedTodos = localStorage.getItem('todos')
+    if (savedTodos) {
+      setTodos(JSON.parse(savedTodos))
+    }
+  })
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
   return (
     <div>
       
