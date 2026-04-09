ae38099646d45c78f07c735e9061e854dce7709b
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-09 21:22:19 +0300

    added functions for adding, toggle and deleting tasks

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -17,6 +17,19 @@ function App() {
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
 
+  const addTodo = (text: string) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false}])
+  }
+
+  const toggleTodo = (id: number) => {
+    setTodos(todos.map(todo) => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo)
+  }
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id)
+  }
+
   return (
     <div>
       
