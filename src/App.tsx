6f1a4d19ecdc727798e7e4d180ae56b1f161473f
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-15 16:22:27 +0300

    created editTodo function

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,10 @@ export default function App() {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  const editTodo = (id: number, newText: string) => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, text: newText } : todo ))
+  }
+
   const filteredTodos = todos.filter(todo => {
     if (filter === 'active') return !todo.completed
     if (filter === 'completed') return todo.completed
