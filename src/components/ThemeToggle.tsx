253239000e6c21074bc8e2d2a9d7271458f59782
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-15 16:46:24 +0300

    created ThemeToggle component

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,11 @@
+import { Moon, Sun } from 'lucide-react'
+import type { Theme } from '../types'
+
+interface ThemeToggleProps {
+  theme: Theme
+  onToggle: () => void
+}
+
+export default function ThemeToggle({theme, onToggle}: ThemeToggleProps) => {
+  return ()
+}
