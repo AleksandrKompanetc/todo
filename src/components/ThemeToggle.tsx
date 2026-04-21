a4a4d3237ef0113a2fb4cc4b8bd5dc007d8087b8
AleksandrKompanetc <moneymacker7@gmail.com>
2026-04-21 23:37:51 +0300

    changed  ThemeToggle component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -6,6 +6,13 @@ interface Props {
   onToggle: () => void
 }
 
-export default function ThemeToggle({isDark, onToggle}: Props) => {
-  return ()
+export default function ThemeToggle({ isDark, onToggle }: Props) => {
+  return (
+    <button
+      onClick={onToggle}
+      className='p-3 rounded-2xl bg-gray-200 dark:bg-gray-700 text-gray-800'
+    >
+      {isDark ? <Sun size={24} /> : <Moon size={24} />}
+    </button>
+  )
 }
