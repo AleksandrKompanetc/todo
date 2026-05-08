6e31021a7fd5f695389197ec7e405febbe600597
AleksandrKompanetc <moneymacker7@gmail.com>
2026-05-08 22:25:08 +0300

    created ThemeToggle component

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,11 +1,18 @@
-import { useState, useEffect } from 'react'
 import { Moon, Sun } from 'lucide-react'
 import type { Theme } from '../types'
 
-export default function ThemeToggle() {
-  return (
-    <button>
+interface Props {
+  isDark: boolean
+  onToggle: () => void
+}
 
+export default function ThemeToggle({ isDark, onToggle }: Props) {
+  return (
+    <button
+      onClick={onToggle}
+      className='p-3 rounded-2xl bg-gray-200 dark:bg-gray-700 text-gray-800'
+    >
+      {isDark ? <Sun size={24} /> : <Moon size={24} />}
     </button>
   )
 }
