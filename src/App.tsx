73389cd5537ed7c4d90b197ef094669c55789a44
AleksandrKompanetc <moneymacker7@gmail.com>
2026-06-08 22:49:25 +0300

    refactor: streamline code structure and improve readability in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import TodoForm from './components/TodoForm'
 import ThemeToggle from './components/ThemeToggle'
 import Filters from './components/Filters'
 import TodoList from './components/TodoList'
-import type { Todo, Filter, Theme } from './types'
 import TodoStats from './components/TodoStats'
+import type { Todo, Filter, Theme } from './types'
+
 
 export default function App() {
   const [todos, setTodos] = useState<Todo[]>([])
